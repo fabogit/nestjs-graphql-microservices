d50570eb8867740b168e086d313a85e98833dfd6
fabogit <fabo.dev.email@gmail.com>
2025-02-23 23:48:50 +0100

    #3
    users graphql resource

---
+++ apps/users/src/users.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import {
+  ApolloFederationDriverConfig,
+  ApolloFederationDriver,
+} from '@nestjs/apollo';
+import { UsersService } from './users.service';
+import { UsersResolver } from './users.resolver';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+      driver: ApolloFederationDriver,
+      autoSchemaFile: {
+        federation: 2,
+      },
+    }),
+  ],
+  providers: [UsersResolver, UsersService],
+})
+export class UsersModule {}
